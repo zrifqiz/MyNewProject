e459ebede602f73e2bcaad9ed088827f6e8b5c96
renotekno <renotekno416@gmail.com>
2017-08-15 20:32:52 +0700

    Membuka Activity yang telah dibuat

--- app/src/main/java/renotekno/com/mynewproject/MainActivity.java
+++ app/src/main/java/renotekno/com/mynewproject/MainActivity.java
@@ -1,13 +1,32 @@
 package renotekno.com.mynewproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button tombolActivity2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // referensikan button pada layout xml
+        tombolActivity2 = (Button) findViewById(R.id.tombolActivity2);
+
+        // tambah click listener
+        tombolActivity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mulai Activity baru
+                Intent intent = new Intent(MainActivity.this, ActivityKe2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
